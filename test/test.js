15a283b461293d8e542e8931b3c73d3354fe86e1
shilili1 <shilili1@jd.com>
2022-08-08 20:29:44 +0800

    feat: 修改去掉的代码

--- test/test.js
+++ test/test.js
@@ -4,92 +4,100 @@ const fs = require('fs');
 
 const openAPI = require('../dist/index');
 
-const gen = async () => {
-  await openAPI.generateService({
-    schemaPath: `${__dirname}/example-files/swagger-get-method-params-convert-obj.json`,
-    serversPath: './servers',
-  });
+// const gen = async () => {
+//   await openAPI.generateService({
+//     schemaPath: `${__dirname}/example-files/swagger-get-method-params-convert-obj.json`,
+//     serversPath: './servers',
+//   });
 
-  await openAPI.generateService({
-    schemaPath: `${__dirname}/example-files/swagger-file-convert.json`,
-    serversPath: './file-servers',
-  });
+//   await openAPI.generateService({
+//     schemaPath: `${__dirname}/example-files/swagger-file-convert.json`,
+//     serversPath: './file-servers',
+//   });
 
-  await openAPI.generateService({
-    requestLibPath: "import request  from '@/request';",
-    schemaPath: `${__dirname}/example-files/swagger-custom-hook.json`,
-    serversPath: './servers',
-    hook: {
-        // 自定义类名
-        customClassName: (tagName) => {
-            return /[A-Z].+/.exec(tagName);
-        },
-        // 自定义函数名
-        customFunctionName: (data) => {
-            let funName = data.operationId ? data.operationId : '';
-            const suffix = 'Using';
-            if (funName.indexOf(suffix) != -1) {
-                funName = funName.substring(0, funName.lastIndexOf(suffix));
-            }
-            return funName;
-        },
-        // 自定义类型名
-        customTypeName: (data) => {
-          const { operationId } = data;
-          const funName = operationId
-            ? operationId[0].toUpperCase() + operationId.substring(1)
-            : '';
-          const tag = operationObject.tags && operationObject.tags[0];
+//   await openAPI.generateService({
+//     requestLibPath: "import request  from '@/request';",
+//     schemaPath: `${__dirname}/example-files/swagger-custom-hook.json`,
+//     serversPath: './servers',
+//     hook: {
+//         // 自定义类名
+//         customClassName: (tagName) => {
+//             return /[A-Z].+/.exec(tagName);
+//         },
+//         // 自定义函数名
+//         customFunctionName: (data) => {
+//             let funName = data.operationId ? data.operationId : '';
+//             const suffix = 'Using';
+//             if (funName.indexOf(suffix) != -1) {
+//                 funName = funName.substring(0, funName.lastIndexOf(suffix));
+//             }
+//             return funName;
+//         },
+//         // 自定义类型名
+//         customTypeName: (data) => {
+//           const { operationId } = data;
+//           const funName = operationId
+//             ? operationId[0].toUpperCase() + operationId.substring(1)
+//             : '';
+//           const tag = operationObject.tags && operationObject.tags[0];
 
-          return `${tag ? tag : ''}${funName}`;
-        }
-    }
-  });
+//           return `${tag ? tag : ''}${funName}`;
+//         }
+//     }
+//   });
 
-  // check 文件生成
-  const fileControllerStr = fs.readFileSync(path.join(__dirname, 'file-servers/api/fileController.ts'), 'utf8');
-  assert(fileControllerStr.indexOf('!(item instanceof File)') > 0);
-  assert(fileControllerStr.indexOf(`requestType: 'form',`) > 0);
-  assert(fileControllerStr.indexOf('Content-Type') < 0);
-  // await openAPI.generateService({
-  //   // requestLibPath: "import request  from '@/request';",
-  //   schemaPath: `http://82.157.33.9/swagger/swagger.json`,
-  //   serversPath: './servers',
-  // });
-  // await openAPI.generateService({
-  //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-  //   serversPath: './servers',
-  //   mockFolder: './mocks',
-  // });
-  // await openAPI.generateService({
-  //   schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
-  //   serversPath: './servers',
-  //   mockFolder: './mocks',
-  // });
-  // await openAPI.generateService({
-  //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/LyDMjDyIhK/1611471979478-opa.json',
-  //   serversPath: './servers',
-  //   mockFolder: './mocks',
-  // });
-  // await openAPI.generateService({
-  //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/Zd7dLTHUjE/ant-design-pro.json',
-  //   serversPath: './servers',
-  //   mockFolder: './mocks',
-  // });
-  // await openAPI.generateService({
-  //   schemaPath: `${__dirname}/morse-api.json`,
-  //   serversPath: './servers',
-  //   mockFolder: './mocks',
-  // });
-  // await openAPI.generateService({
-  //   schemaPath: `${__dirname}/oc-swagger.json`,
-  //   serversPath: './servers',
-  //   mockFolder: './mocks',
-  // });
-  // await openAPI.generateService({
-  //   schemaPath: `${__dirname}/java-api.json`,
-  //   serversPath: './servers',
-  //   mockFolder: './mocks',
-  // });
-};
-gen();
+//   // check 文件生成
+//   const fileControllerStr = fs.readFileSync(path.join(__dirname, 'file-servers/api/fileController.ts'), 'utf8');
+//   assert(fileControllerStr.indexOf('!(item instanceof File)') > 0);
+//   assert(fileControllerStr.indexOf(`requestType: 'form',`) > 0);
+//   assert(fileControllerStr.indexOf('Content-Type') < 0);
+//   // await openAPI.generateService({
+//   //   // requestLibPath: "import request  from '@/request';",
+//   //   schemaPath: `http://82.157.33.9/swagger/swagger.json`,
+//   //   serversPath: './servers',
+//   // });
+//   // await openAPI.generateService({
+//   //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
+//   //   serversPath: './servers',
+//   //   mockFolder: './mocks',
+//   // });
+//   // await openAPI.generateService({
+//   //   schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
+//   //   serversPath: './servers',
+//   //   mockFolder: './mocks',
+//   // });
+//   // await openAPI.generateService({
+//   //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/LyDMjDyIhK/1611471979478-opa.json',
+//   //   serversPath: './servers',
+//   //   mockFolder: './mocks',
+//   // });
+//   // await openAPI.generateService({
+//   //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/Zd7dLTHUjE/ant-design-pro.json',
+//   //   serversPath: './servers',
+//   //   mockFolder: './mocks',
+//   // });
+//   // await openAPI.generateService({
+//   //   schemaPath: `${__dirname}/morse-api.json`,
+//   //   serversPath: './servers',
+//   //   mockFolder: './mocks',
+//   // });
+//   // await openAPI.generateService({
+//   //   schemaPath: `${__dirname}/oc-swagger.json`,
+//   //   serversPath: './servers',
+//   //   mockFolder: './mocks',
+//   // });
+//   // await openAPI.generateService({
+//   //   schemaPath: `${__dirname}/java-api.json`,
+//   //   serversPath: './servers',
+//   //   mockFolder: './mocks',
+//   // });
+// };
+// gen();
+
+
+openAPI.generateService({
+  schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json',
+  serversPath: './src/servers',
+  mockFolder: './src/mock/api',
+  // apiPrefix: '',
+});
